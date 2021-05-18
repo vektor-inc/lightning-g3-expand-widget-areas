ce6e79820c09fe7e3749d1d0a46717deac23cf33
kurudrive <kurudrive@gmail.com>
2021-05-19 01:08:02 +0900

    first commit

---
+++ lightning-g3-expand-widget-areas.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Lightning G3 Expand Widget Areas
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     lightning-g3-expand-widget-areas
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Lightning_G3_Expand_Widget_Areas
+ */
+
+// Your code starts here.
